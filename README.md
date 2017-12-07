4cd80519c3c87c3ce812f0a3d31f6d027999ba37
Te Vallee <astronomemusic@gmail.com>
2017-12-06 15:34:45 -1000

    initial commit. webpack implemented

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Nav-Cart-Counter
+
+## Development
+
+install dependencies
+
+```
+yarn install
+```
+
+start dev server
+
+```
+yarn run dev
+```
+
+this serves `src/` on `localhost:9000`
+
+## Build
+
+```
+yarn build
+```
+
+necessary files will build into `dist/`
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'nav-cart-counter.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MinifyPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
