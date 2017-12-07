ef630059e7ecc24a901f59dbae399645aa6be69f
Te Vallee <astronomemusic@gmail.com>
2017-12-06 16:33:26 -1000

    cart notification displays item added to cart

--- src/app.js
+++ src/app.js
@@ -1,19 +1,39 @@
-const cartCounter = document.getElementById('nav-cart-counter');
+const cartCounter = document.getElementById('nav-cart-counter'),
+      hiddenClass = 'nav-cart-counter-hidden';
 let cartCount = getCartCount();
+let cartLink = document.createElement('a');
+let cartNotification = document.createElement('span');
+let cartNotificationText = document.createElement('span');
 
 if(cartCounter) {
   let addToCartButtons = document.querySelectorAll('button[data-aerostat-id]');
   Array.prototype.slice.call(addToCartButtons).forEach((button) => button.onclick = updateCartCount);
-  let cartLink = document.createElement('a');
+
   cartLink.className = 'nav-cart-counter-count';
   cartLink.href = '/cart';
   cartCounter.appendChild(cartLink);
-  cartCounter.innerHTML = cartCount;
+  cartLink.innerHTML = cartCount;
+
+  cartNotification.className = `nav-cart-counter-notification ${hiddenClass}`;
+  cartCounter.appendChild(cartNotification);
+
+  cartNotificationText.className = 'nav-cart-counter-notification-text';
+  cartNotification.appendChild(cartNotificationText);
 }
 
-function updateCartCount() {
+function notify(dataset) {
+  cartNotificationText.innerHTML = `${dataset.productTitle} - ${dataset.productVariationTitle} added to cart.`;
+  cartNotification.classList.toggle(hiddenClass);
+  setTimeout(() => {
+    cartNotification.classList.toggle(hiddenClass);
+    cartNotificationText.innerHTML = '';
+  },3000);
+}
+
+function updateCartCount(e) {
   cartCount++;
-  cartCounter.innerHTML = cartCount;
+  cartLink.innerHTML = cartCount;
+  notify(e.srcElement.dataset);
 }
 
 function getCartCount() {
