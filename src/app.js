fee96052a8a59e7ee3c3896b0d82bb3519488dbc
Te Vallee <astronomemusic@gmail.com>
2017-12-06 15:44:16 -1000

    click event listener added to all Add to Cart buttons

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+const cartCounter = document.getElementById("nav-cart-counter");
+
+if(cartCounter) {
+  let addToCartButtons = document.querySelectorAll('button[data-aerostat-id]');
+  Array.prototype.slice.call(addToCartButtons).forEach((button) => button.onclick = updateCartCount);
+}
+
+function updateCartCount() {
+  console.log('click')
+}
