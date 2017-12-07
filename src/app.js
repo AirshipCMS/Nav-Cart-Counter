b77123e701295dfd62150e110310fb02ed512bf8
Te Vallee <astronomemusic@gmail.com>
2017-12-06 16:01:30 -1000

    clicking Add to Cart button increments cart count

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
-const cartCounter = document.getElementById("nav-cart-counter");
+const cartCounter = document.getElementById('nav-cart-counter');
+let cartCount = getCartCount();
 
 if(cartCounter) {
   let addToCartButtons = document.querySelectorAll('button[data-aerostat-id]');
   Array.prototype.slice.call(addToCartButtons).forEach((button) => button.onclick = updateCartCount);
+  let cartLink = document.createElement('a');
+  cartLink.className = 'nav-cart-counter-count';
+  cartLink.href = '/cart';
+  cartCounter.appendChild(cartLink);
+  cartCounter.innerHTML = cartCount;
 }
 
 function updateCartCount() {
-  console.log('click')
+  cartCount++;
+  cartCounter.innerHTML = cartCount;
+}
+
+function getCartCount() {
+  return JSON.parse(localStorage.getItem('cart')).items.length || 0;
 }
