3731fb8c0a2bbf409132de6002785ce30ac6c4d2
Te Vallee <astronomemusic@gmail.com>
2017-12-06 16:41:30 -1000

    styled cart counter

--- src/app.js
+++ src/app.js
@@ -1,23 +1,25 @@
-const cartCounter = document.getElementById('nav-cart-counter'),
-      hiddenClass = 'nav-cart-counter-hidden';
+const cartCounter = document.getElementById("nav-cart-counter"),
+  hiddenClass = "nav-cart-counter-hidden";
 let cartCount = getCartCount();
-let cartLink = document.createElement('a');
-let cartNotification = document.createElement('span');
-let cartNotificationText = document.createElement('span');
+let cartLink = document.createElement("a");
+let cartNotification = document.createElement("span");
+let cartNotificationText = document.createElement("span");
 
-if(cartCounter) {
-  let addToCartButtons = document.querySelectorAll('button[data-aerostat-id]');
-  Array.prototype.slice.call(addToCartButtons).forEach((button) => button.onclick = updateCartCount);
+if (cartCounter) {
+  let addToCartButtons = document.querySelectorAll("button[data-aerostat-id]");
+  Array.prototype.slice
+    .call(addToCartButtons)
+    .forEach(button => (button.onclick = updateCartCount));
 
-  cartLink.className = 'nav-cart-counter-count';
-  cartLink.href = '/cart';
+  cartLink.className = "nav-cart-counter-count";
+  cartLink.href = "/cart";
   cartCounter.appendChild(cartLink);
   cartLink.innerHTML = cartCount;
 
   cartNotification.className = `nav-cart-counter-notification ${hiddenClass}`;
   cartCounter.appendChild(cartNotification);
 
-  cartNotificationText.className = 'nav-cart-counter-notification-text';
+  cartNotificationText.className = "nav-cart-counter-notification-text";
   cartNotification.appendChild(cartNotificationText);
 }
 
@@ -26,8 +28,8 @@ function notify(dataset) {
   cartNotification.classList.toggle(hiddenClass);
   setTimeout(() => {
     cartNotification.classList.toggle(hiddenClass);
-    cartNotificationText.innerHTML = '';
-  },3000);
+    cartNotificationText.innerHTML = "";
+  }, 3000);
 }
 
 function updateCartCount(e) {
@@ -37,5 +39,5 @@ function updateCartCount(e) {
 }
 
 function getCartCount() {
-  return JSON.parse(localStorage.getItem('cart')).items.length || 0;
+  return JSON.parse(localStorage.getItem("cart")).items.length || 0;
 }
